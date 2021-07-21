f25a75a60946a525e80e213e987ba18338bd6584
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-07-20 22:00:22 -0300

    cria projeto e inicia exercícios

---
+++ Desenvolvimento-Front-End/introdução-react/bloco_11_dia_1/my-app/src/App.js
@@ -0,0 +1,10 @@
+import MyComponent from './Component';
+import './App.css';
+
+function App() {
+  return (
+    <MyComponent/>
+  );
+}
+
+export default App;
---
+++ Desenvolvimento-Front-End/introdução-react/bloco_11_dia_1/my-app/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Amós dos Santos Rodrigues</h1>
+        <p>Student Development Web at Trybe</p>
+      </div>
+    )
+  }
+}
+
+export default MyComponent;
