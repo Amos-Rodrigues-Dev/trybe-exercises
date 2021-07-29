3936311b02f04c7c0c0b22a6b1eda9a6a776adc7
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-07-29 18:53:08 -0300

    adiciona segundo projeto para exercise

---
+++ Desenvolvimento-Front-End/ciclo-componentes-react-router/bloco_13_dia_1/my-interdimensional-app/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        characters: [],
+    };
+    this.fetchCharacters = this.fetchCharacters.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCharacters();
+  }
+
+  fetchCharacters = () => {
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({characters: data.results})
+    })
+  }
+
+  async fetchCharacterAsync() {
+    const response = await fetch('https://rickandmortyapi.com/api/character');
+    const data = await response.json();
+    this.setState({characters: data.results})
+  }
+
+  render() {
+    const { characters } = this.state;
+    return (
+      <div className="App">
+        <h1>
+          Ricky and Morty Characters:
+        </h1>
+        <div className="body">
+          {characters.map(({ name, image}) => {
+            return (
+              <div className="container" key={name}>
+                <h3>{name}</h3>
+                <img src={image} alt={name} />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
