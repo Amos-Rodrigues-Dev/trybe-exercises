6f11b53d098b2cba4c1b7e8647ca4a836a7a5a02
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-08-26 10:11:08 -0300

    implemta tela de Login e Cadastro

--- Desenvolvimento-Front-End/gerenciamento-estado-redux/bloco_16_dia_3/my-app-redux/src/App.js
+++ Desenvolvimento-Front-End/gerenciamento-estado-redux/bloco_16_dia_3/my-app-redux/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Cadastro from './pages/cadastro';
+import Clientes from './pages/clientes';
+import Home from './pages/home';
+import Login from './pages/login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route path="/login" component={ Login } />
+        <Route path="/clientes" component={ Clientes } />
+        <Route path="/cadastro" component={ Cadastro } />
+      </Switch>
     </div>
   );
 }
