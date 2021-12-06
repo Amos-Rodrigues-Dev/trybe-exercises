c2167563a3e5907cff7a4ad172196c70d7465aba
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-12-06 14:47:47 -0300

    realiza exercises de fixacao

---
+++ Desenvolvimento-Back-End/bloco_26-introducao-nodejs/dia-5-express-middlewares/testeErrorMiddleware.js
@@ -0,0 +1,29 @@
+/* index.js */
+const express = require('express');
+const rescue = require('express-rescue');
+const errorMiddleware = require('./errorMiddleware');
+const fs = require('fs/promises');
+
+const app = express();
+
+
+app.get('/:fileName', [
+  rescue( async (req, res, next) => {
+    const file = await fs.readFile(`./${req.params.fileName}`);
+    res.send(file.toString('utf-8'));
+  }),
+  (err, req, res, next) => {
+    if (err.code === 'ENOENT') {
+      const newError = new Error(err.message);
+      newError.code = 'file_not_found';
+      newError.status = 404;
+      return next(newError);
+    }
+
+    return next(err);
+  },
+]);
+
+app.use(errorMiddleware);
+
+app.listen(3003);
