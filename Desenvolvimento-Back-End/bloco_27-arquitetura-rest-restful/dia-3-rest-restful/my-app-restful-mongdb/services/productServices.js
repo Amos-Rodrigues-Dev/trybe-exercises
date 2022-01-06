28728cd661b2603f81f45070adb0d01d683c4d20
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-01-06 10:05:58 -0300

    realiza exercise de fixação parte 2

--- Desenvolvimento-Back-End/bloco_27-arquitetura-rest-restful/dia-3-rest-restful/my-app-restful-mongdb/services/productServices.js
+++ Desenvolvimento-Back-End/bloco_27-arquitetura-rest-restful/dia-3-rest-restful/my-app-restful-mongdb/services/productServices.js
@@ -15,8 +15,54 @@ async function getById(id) {
     throw { status: 404, message: 'Identificador Inválido' };
 
   const product = await ProductModel.getById(id);
-  if (!product) throw { status: 404, message: 'Produto não encontrado' };
+  if (!product) throw { status: 404, message: 'Product not found' };
   return product;
 }
 
-module.exports = { getAll, getById };
+async function add(name, brand) {
+  const productExists = await ProductModel.getByName(name);
+
+  if (productExists) throw { status: 400, message: 'Product already exists' };
+  console.log(productExists);
+
+  const productId = await ProductModel.add(name, brand);
+
+  const newProduct = {
+    id: productId,
+    name,
+    brand
+  };
+  return newProduct;
+}
+
+async function exclude(id) {
+  if (!ObjectId.isValid(id))
+    throw { status: 404, message: 'Identificador Inválido' };
+
+  const product = await ProductModel.getById(id);
+
+  if (!product) throw { status: 400, message: 'Product not exists' };
+
+  await ProductModel.exclude(id);
+  return product;
+}
+
+async function update(id, name, brand) {
+  if (!ObjectId.isValid(id))
+    throw { status: 404, message: 'Identificador Inválido' };
+
+  const product = await ProductModel.getById(id);
+
+  if (!product) throw { status: 400, message: 'Product not exists' };
+
+  const alteredProduct = await ProductModel.update(id, name, brand);
+  return alteredProduct;
+
+  // const product = await db
+  //   .collection('products')
+  //   .updateOne({ _id: ObjectID(id) }, { $set: name, brand });
+  // if (!product) return add(name, brand);
+  // return product;
+}
+
+module.exports = { getAll, getById, add, exclude, update };
