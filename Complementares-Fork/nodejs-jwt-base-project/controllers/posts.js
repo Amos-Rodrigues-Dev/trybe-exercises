e7f49f0389277243a1819250936e5c3970bca853
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-01-14 18:16:51 -0300

    remove diretorio do exercise 28.1

--- Complementares-Fork/nodejs-jwt-base-project/controllers/posts.js
+++
@@ -1,6 +0,0 @@
-const { Post } = require('../models');
-
-module.exports = async (req, res) => {
-  const posts = await Post.findAll({ attributes: { exclude: 'id' } });
-  res.status(200).json({ mockPosts: posts });
-};
