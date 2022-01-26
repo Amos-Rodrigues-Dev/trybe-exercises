e33085d2297a3591eba05e63a88dd8d5e7787c53
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-01-26 10:00:01 -0300

    finaliza exercises da aula

--- Desenvolvimento-Back-End/bloco-29-deploy/dia-2-deploy-process-managers/pm2-example/index.js
+++ Desenvolvimento-Back-End/bloco-29-deploy/dia-2-deploy-process-managers/pm2-example/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const process = require('process');
 
 const app = express();
 
@@ -8,5 +9,11 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Inciando Process Managers'));
+
+app.get('/break', (req, res) => {
+  res.send('Quebrando...');
+
+  process.exit(1);
+});
 
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
