b676b7242dde728bfd2e3609ba94d872ac6387a0
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-01-28 22:18:01 -0300

    inicia exercises da aula

---
+++ Desenvolvimento-Back-End/bloco-30-arquitetura-solid-orm/dia-1-principios-soid/exercise/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+const controller = require('./controllers/controllerPlants');
+const errorMiddleware = require('./middlewares/errorMiddleware');
+
+app.use(bodyParser.json());
+
+app.post('/plants', controller.salvePlants);
+app.get('/plants', controller.getAllPlants);
+app.get('/plant/:id', controller.getPlantById);
+app.delete('/plant/:id', controller.removePlantById);
+app.post('/plant', controller.createNewPlant);
+
+app.use(errorMiddleware);
+
+app.listen(PORT, () => console.log(`Escutado na porta ${PORT}`));
