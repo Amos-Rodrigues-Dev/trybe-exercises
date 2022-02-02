101669a1c5b3cae2ec123dc22fe874a9d7e1e83f
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-02-02 00:27:49 -0300

    finaliza exercises de fixacao

--- Desenvolvimento-Back-End/bloco-30-arquitetura-solid-orm/dia-2-orm/app-with-sequelize/src/models/user.js
+++ Desenvolvimento-Back-End/bloco-30-arquitetura-solid-orm/dia-2-orm/app-with-sequelize/src/models/user.js
@@ -6,10 +6,12 @@ const User = (sequelize, DataTypes) => {
       email: DataTypes.STRING,
       phone_num: DataTypes.STRING,
     },
-    // {
-    //   freezeTableName: true, // utilizado para definir o nome da tabela exatamento igual ao que estar no model ou
-    // tableName: "Xablau"
-    // },
+    {
+      //   freezeTableName: true, // utilizado para definir o nome da tabela exatamento igual ao que estar no model ou
+      // tableName: "Xablau"
+      // underscored: true, //Este campo nos ajudará a resolver o primeiro problema, ele fará com que parâmetros recebidos em Camel Case , sejam convertidos em Snake Case , quando for feita uma consulta a respectiva tabela.
+      // tableName: 'Users', // retirando assim a responsabilidade do Sequelize de nomear a tabela.
+    },
   );
 
   return User;
