be6b20752a324424990f94b31e24e6b9792413d5
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-04-08 14:37:43 -0300

    chore: add file with exercise

---
+++ Ciencia-da-Computacao/bloco-34-redes-raspagem-dados/dia-3-raspagem/exercises/exercise02.py
@@ -0,0 +1,14 @@
+import requests
+
+# Exercício 2 Faça uma requisição ao recurso usuários da API do Github
+# ( https://api.github.com/users ),
+# exibindo o username e url de todos os usuários retornados.
+# mojombo https://api.github.com/users/mojombo
+# defunkt https://api.github.com/users/defunkt
+# pjhyett https://api.github.com/users/pjhyett
+# wycats https://api.github.com/users/wycats
+
+response = requests.get("https://api.github.com/users")
+users = response.json()
+for user in users:
+    print(f"{user['login']} {user['url']}")
